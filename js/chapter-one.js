4d459a44b215e47e54910d7120695d294bd25c51
swati <swatidash2007@gmail.com>
2020-02-22 12:52:16 -0800

    created chapter-one.js file

---
+++ js/chapter-one.js
@@ -0,0 +1,16 @@
+//setting and swaping
+
+
+//Print -52 to 1066
+
+for (var i=-52; i<=1066; i++){
+  console.log(i);
+}
+
+//Don't Worry, Be Happy.Create beCheerful(), call "good morning 98 times"
+
+function beCheerful(){
+  for(var i=1; i<=98; i++){
+    console.log("good morning!");
+  }
+}
